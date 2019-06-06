ba1f666efe6310f8b400b9870e4f6d65c6ea1aef
Gar <gartoyou@gmail.com>
2019-06-06 05:35:01 +0000

    add mongoDB Atlas

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v10", {useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp_v10", {useNewUrlParser: true });
+mongoose.connect("mongodb+srv://Gar:gar@cluster0-eaolb.mongodb.net/test?retryWrites=true&w=majority", {userNewUrlParser: true, useCreateIndex: true})
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
