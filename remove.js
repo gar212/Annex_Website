aeb40a88a5fa0f63bff789f83e76c19bc3ef5aca
Gar <gartoyou@gmail.com>
2019-06-06 05:10:27 +0000

    initial deploy commit

---
+++ remove.js
@@ -0,0 +1,15 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+
+function seedDB(){
+    Campground.remove({}, function (err){
+    if(err){
+        console.log(err);
+    } else {
+        console.log("Removed!");
+    }
+});
+    
+}
+
+module.exports = seedDB;
